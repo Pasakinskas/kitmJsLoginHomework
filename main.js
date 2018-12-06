772eefcfe145d2a3f497ba1a58ebb00099118336
Marius Pasakinskas <marius.pasakinskas@gmail.com>
2018-12-06 12:31:42 +0200

    commit for evaluation

---
+++ main.js
@@ -0,0 +1,70 @@
+const app = new Vue({
+    el: "#app",
+    data: () => ({
+        pageTitle: "Please login to view student data",
+        username: "",
+        password: "",
+        isAuthorized: false,
+        badLoginAttempted: false,
+        students: [],
+        tableFields: [
+            {name: "name"},
+            {name: "age"},
+            {name: "picture", type: "picture"},
+            {name: "balance", type: "currency"},
+            {name: "tags", type: "list"},
+            {name: "registered"},
+            {name: "favoriteFruit"},
+        ],
+        dollarsToAdd: 1000,
+    }),
+    methods: {
+        async login() {
+            try {
+                await this.authorize();
+                this.isAuthorized = true;
+                this.pageTitle = "Welcome to the student dashboard";
+                this.fetchStudents();
+            }
+            catch (err) {
+                this.badLoginAttempted = true;
+            }
+        },
+        async authorize() {
+            const url = "http://kitmjavascript.azurewebsites.net/api/login";
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "username": this.username,
+                    "password": this.password,
+                })
+            });
+            const json = await res.json();
+            if (!json.isSuccessStatusCode) {
+                throw new Error(json.statusCode);
+            }
+        },
+        async fetchStudents() {
+            const res = await fetch("http://kitmjavascript.azurewebsites.net/api/studentai");
+            this.students = await res.json();
+        },
+        modifyBalance(amount) {
+            const studentMoney = JSON.parse(amount.toString().replace(/[$,]+/g, ""));
+            const formatter = new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+                minimumFractionDigits: 2
+            });
+
+            return formatter.format(studentMoney + this.dollarsToAdd);
+        },
+    },
+    computed: {
+        activeStudents() {
+            return this.students.filter(student => student.isActive);
+        }
+    }
+});
